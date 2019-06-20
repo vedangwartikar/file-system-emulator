261a80d0b12b56e410a4fcfb2a8ce91fcf092b85
Vedang Wartikar <vedangwartikar17@gmail.com>
2019-06-20 22:09:20 +0530

    update till write

--- vfs.c
+++ vfs.c
@@ -65,113 +65,6 @@ UFDT UFDTArr[50];
 SUPERBLOCK SUPERBLOCKobj;
 PINODE head = NULL;
 
-int main()
-{
-    char *ptr = NULL;
-    int ret = 0, fd = 0, count = 0;
-    char command[4][80], str[80], arr[1024];
-
-    InitializeSuperBlock();
-    CreateDILB();
-
-    while(1)
-    {
-        fflush(stdin);
-        strcpy(str, "");
-
-        printf("Virtual File System: > ");
-        fgets(str, 80, stdin);
-
-        count = sscanf(str, "%s%s%s%s", command[0], command[1], command[2], command[3]);
-
-        if(count == 1)
-        {
-            if(_stricmp(command[0], "ls") == 0)
-            {
-                ls_file();
-            }
-            else if(_stricmp(command[0], "closeall") == 0)
-            {
-                CloseAllFile();
-                printf("All files closed successfully\n");
-                continue;
-            }
-            else if(_stricmp(command[0], "clear") == 0)
-            {
-                system("cls");
-                continue;
-            }
-            else if(_stricmp(command[0], "help") == 0)
-            {
-                DisplayHelp();
-                continue;
-            }
-            else if(_stricmp(command[0], "exit") == 0)
-            {
-                printf("Terminating the Virtual File System\n");
-                break;
-            }
-            else
-            {
-                printf("ERROR: Command not found!!!\n");
-                continue;
-            }
-        }
-        else if(count == 2)
-        {
-            if(_stricmp(command[0], "stat") == 0)
-            {
-                ret = stat_file(command[1]);
-
-                if(ret == -1)
-                {
-                    printf("ERROR: Incorrect parameters!!!\n");
-                }
-                if(ret == -2)
-                {
-                    printf("ERROR: There is no such file!!!\n");
-                }
-                continue;
-            }
-            else if(_stricmp(command[0], "fstat") == 0)
-            {
-                ret = fstat_file(atoi(command[1]));
-
-                if(ret == -1)
-                {
-                    printf("ERROR: Incorrect parameters!!!\n");
-                }
-                if(ret == -2)
-                {
-                    printf("ERROR: There is no such file!!!\n");
-                }
-                continue;
-            }
-            else if(_stricmp(command[0], "close" == 0))
-            {
-                ret = CloseFileByName(command[1]);
-
-                if(ret == -1)
-                {
-                    printf("ERROR: There is no such file!!!\n");
-                }
-                continue;
-            }
-            else if(_stricmp(command[0], "rm" == 0))
-            {
-                ret = rm_File(command[1]);
-                if(ret == -1)
-                {
-                    printf("ERROR: There is no such file!!!\n");
-                }
-                continue;
-            }
-
-        }
-    }
-    return 0;
-}
-
 void InitializeSuperBlock()
 {
     int i = 0;
@@ -419,4 +312,248 @@ int rm_File(char *name)
 
      UFDTArr[fd].ptrfiletable = NULL;
     (SUPERBLOCKobj.FreeInodes)++;
+}
+
+void man(char *name)
+{
+    if(name == NULL)
+    {
+        return;
+    }
+
+    if(_stricmp(name, "create") == 0)
+    {
+        printf("Description: Used to Create new regular file\n");
+        printf("Usage: create File_name Permission\n");
+    }
+    else if(_stricmp(name, "read") == 0)
+    {
+        printf("Description: Used to read data from a regular file\n");
+        printf("Usage: read File_name No_of_bytes_to_read\n");
+    }
+    else if(_stricmp(name, "write") == 0)
+    {
+        printf("Description: Used to write data into a regular file\n");
+        printf("Usage: write File_name\n");
+    }
+    else if(_stricmp(name, "ls") == 0)
+    {
+        printf("Description: Used to list all information about file\n");
+        printf("Usage: ls\n");
+    }
+    else if(_stricmp(name, "stat") == 0)
+    {
+        printf("Description: Used to display information of a file\n");
+        printf("Usage: stat File_name\n");
+    }
+    else if(_stricmp(name, "fstat") == 0)
+    {
+        printf("Description: Used to display information of a file\n");
+        printf("Usage: stat File_descriptor\n");
+    }
+    else if(_stricmp(name, "truncate") == 0)
+    {
+        printf("Description: Used to remove all the data from a file\n");
+        printf("Usage: truncate File_name\n");
+    }
+    else if(_stricmp(name, "open") == 0)
+    {
+        printf("Description: Used open existing file\n");
+        printf("Usage: open File_name mode\n");
+    }
+    else if(_stricmp(name, "close") == 0)
+    {
+        printf("Description: Used to close an opened file\n");
+        printf("Usage: close File_name\n");
+    }
+    else if(_stricmp(name, "closeall") == 0)
+    {
+        printf("Description: Used to close all the files\n");
+        printf("Usage: closeall\n");
+    }
+    else if(_stricmp(name, "lseek") == 0)
+    {
+        printf("Description: Used to change file offset\n");
+        printf("Usage: lseek File_name Change_in_Offset Start_Point\n");
+    }
+    else if(_stricmp(name, "rm") == 0)
+    {
+        printf("Description: Used to delete a specific file\n");
+        printf("Usage: rm File_name\n");
+    }
+    else
+    {
+        printf("ERROR: No manual entry available!\n");
+    }
+
+}
+
+int WriteFile(int fd, char *arr, int isize)
+{
+    if(((UFDTArr[fd].ptrfiletable->mode) != WRITE && (UFDTArr[fd].ptrfiletable->mode) != READ+WRITE))
+    {
+        return -1;
+    }
+    if(((UFDTArr[fd].ptrfiletable->ptrinode->Permission) != WRITE) && ((UFDTArr[fd].ptrfiletable->ptrinode->Permission) != READ+WRITE))
+    {
+        return -1;
+    }
+    if((UFDTArr[fd].ptrfiletable->writeoffset) == MAXFILESIZE)
+    {
+        return -2;
+    }
+    if((UFDTArr[fd].ptrfiletable->ptrinode->FileType) != REGULAR)
+    {
+        return -3;
+    }
+    strncpy((UFDTArr[fd].ptrfiletable->ptrinode->Buffer) + (UFDTArr[fd].ptrfiletable->writeoffset), arr, isize);
+
+    (UFDTArr[fd].ptrfiletable->writeoffset) = (UFDTArr[fd].ptrfiletable->writeoffset) + isize;
+
+    (UFDTArr[fd].ptrfiletable->ptrinode->FileActualSize) = (UFDTArr[fd].ptrfiletable->ptrinode->FileActualSize) + isize;
+
+    return isize;
+}
+int main()
+{
+    char *ptr = NULL;
+    int ret = 0, fd = 0, count = 0;
+    char command[4][80], str[80], arr[1024];
+
+    InitializeSuperBlock();
+    CreateDILB();
+
+    while(1)
+    {
+        fflush(stdin);
+        strcpy(str, "");
+
+        printf("Virtual File System: > ");
+        fgets(str, 80, stdin);
+
+        count = sscanf(str, "%s%s%s%s", command[0], command[1], command[2], command[3]);
+
+        if(count == 1)
+        {
+            if(_stricmp(command[0], "ls") == 0)
+            {
+                ls_file();
+            }
+            else if(_stricmp(command[0], "closeall") == 0)
+            {
+                CloseAllFile();
+                printf("All files closed successfully\n");
+                continue;
+            }
+            else if(_stricmp(command[0], "clear") == 0)
+            {
+                system("cls");
+                continue;
+            }
+            else if(_stricmp(command[0], "help") == 0)
+            {
+                DisplayHelp();
+                continue;
+            }
+            else if(_stricmp(command[0], "exit") == 0)
+            {
+                printf("Terminating the Virtual File System...\n");
+                break;
+            }
+            else
+            {
+                printf("ERROR: Command not found!!!\n");
+                continue;
+            }
+        }
+        else if(count == 2)
+        {
+            if(_stricmp(command[0], "stat") == 0)
+            {
+                ret = stat_file(command[1]);
+
+                if(ret == -1)
+                {
+                    printf("ERROR: Incorrect parameters!!!\n");
+                }
+                if(ret == -2)
+                {
+                    printf("ERROR: There is no such file!!!\n");
+                }
+                continue;
+            }
+            else if(_stricmp(command[0], "fstat") == 0)
+            {
+                ret = fstat_file(atoi(command[1]));
+
+                if(ret == -1)
+                {
+                    printf("ERROR: Incorrect parameters!!!\n");
+                }
+                if(ret == -2)
+                {
+                    printf("ERROR: There is no such file!!!\n");
+                }
+                continue;
+            }
+            else if(_stricmp(command[0], "close") == 0)
+            {
+                ret = CloseFileByName(command[1]);
+
+                if(ret == -1)
+                {
+                    printf("ERROR: There is no such file!!!\n");
+                }
+                continue;
+            }
+            else if(_stricmp(command[0], "rm") == 0)
+            {
+                ret = rm_File(command[1]);
+                if(ret == -1)
+                {
+                    printf("ERROR: There is no such file!!!\n");
+                }
+                continue;
+            }
+            else if(_stricmp(command[0], "man") == 0)
+            {
+                man(command[1]);
+            }
+            else if(_stricmp(command[0], "write") == 0)
+            {
+                fd = GetFDFromName(command[1]);
+                if(fd == -1)
+                {
+                    printf("ERROR: Incorrect parameters!!!\n");
+                    continue;
+                }
+                printf("Enter the data: \n");
+                scanf("%[^\n]", arr);
+
+                ret = strlen(arr);
+                if(ret == 0)
+                {
+                    printf("ERROR: Incorrect parameters!!!\n");
+                    continue;
+                }
+
+                ret = WriteFile(fd, arr, ret);
+
+                if(ret == -1)
+                {
+                    printf("ERROR: Permission denied!!!\n");
+                }
+                if(ret == -2)
+                {
+                    printf("ERROR: No sufficient memory to write!!!\n");
+                }
+                if(ret == -3)
+                {
+                    printf("ERROR: It is not a regular file!!!\n");
+                }
+            }
+
+        }
+    }
+    return 0;
 }
